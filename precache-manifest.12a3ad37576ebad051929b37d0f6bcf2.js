3d7277c7e44eeb728639718da1d1a10504580c72
Anna Kholod <uashop89@gmail.com>
2020-03-21 13:08:02 +0200

    Updates

---
+++ precache-manifest.12a3ad37576ebad051929b37d0f6bcf2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a47b1095ffe3ce60bf49e40d3f99f0b6",
+    "url": "/flat-fix/index.html"
+  },
+  {
+    "revision": "db948d555dd32cc6c25a",
+    "url": "/flat-fix/static/css/main.ba00b450.chunk.css"
+  },
+  {
+    "revision": "f1bd75172e8199390ef7",
+    "url": "/flat-fix/static/js/2.a89e11ce.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/flat-fix/static/js/2.a89e11ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db948d555dd32cc6c25a",
+    "url": "/flat-fix/static/js/main.a448e4e4.chunk.js"
+  },
+  {
+    "revision": "d3ec7b5d5728cd62bdd5",
+    "url": "/flat-fix/static/js/runtime-main.0684ee01.js"
+  },
+  {
+    "revision": "8cc06abc5bd170376093063a58b099dd",
+    "url": "/flat-fix/static/media/feedback.8cc06abc.jpg"
+  },
+  {
+    "revision": "96abe6c2112985bd9e2b4736d5978810",
+    "url": "/flat-fix/static/media/header.96abe6c2.jpg"
+  },
+  {
+    "revision": "aa7595fcd4d715b5ae37110d42f3ac20",
+    "url": "/flat-fix/static/media/request.aa7595fc.jpg"
+  }
+]);
